7363502402d175e54a8481f492013dffa2add4bc
soanatareportagens <soanatareportagens@gmail.com>
2018-05-02 12:33:50 -0300

    Update msg live.js

--- msgsvivo/msg live.js
+++ msgsvivo/msg live.js
@@ -5,45 +5,6 @@ var verticalpos="fromtop" // "fromtop" o "frombottom" define se vai em cima ou e
 function iecompattest(){
 return (document.compatMode && document.compatMode!="BackCompat")? document.documentElement : document.body
 }
-function closebar(){
-if (persistclose)
-document.cookie="remainclosed=1"
-document.getElementById("topbar").style.visibility="hidden"
-}
-function staticbar(){
-barheight=document.getElementById("topbar").offsetHeight
-var ns = (navigator.appName.indexOf("Netscape") != -1) || window.opera;
-var d = document;
-function ml(id){
-var el=d.getElementById(id);
-if (!persistclose || persistclose && get_cookie("remainclosed")=="")
-el.style.visibility="visible"
-if(d.layers)el.style=el;
-el.sP=function(x,y){this.style.left=x+"px";this.style.top=y+"px";};
-el.x = startX;
-if (verticalpos=="fromtop")
-el.y = startY;
-else{
-el.y = ns ? pageYOffset + innerHeight : iecompattest().scrollTop +  iecompattest().clientHeight;
-el.y -= startY;
-}
-return el;
-}
-window.stayTopLeft=function(){
-if (verticalpos=="fromtop"){
-var pY = ns ? pageYOffset : iecompattest().scrollTop;
-ftlObj.y += (pY + startY - ftlObj.y)/8;
-}
-else{
-var pY = ns ? pageYOffset + innerHeight - barheight: iecompattest().scrollTop + iecompattest().clientHeight - barheight;
-ftlObj.y += (pY - startY - ftlObj.y)/8;
-}
-ftlObj.sP(ftlObj.x, ftlObj.y);
-setTimeout("stayTopLeft()", 10);
-}
-ftlObj = ml("topbar");
-stayTopLeft();
-}
 if (window.addEventListener)
 window.addEventListener("load", staticbar, false)
 else if (window.attachEvent)
