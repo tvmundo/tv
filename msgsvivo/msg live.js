a803fe34ccdf7065ca670c44f85d0f0fed41072b
soanatareportagens <soanatareportagens@gmail.com>
2018-05-02 11:31:59 -0300

    Update msg live.js

--- msgsvivo/msg live.js
+++ msgsvivo/msg live.js
@@ -1,3 +1,75 @@
-<div id="Layer1" style="display:none; height:90px; width:100%px; background:#FF9900; top:15px;left:40px">
-<span style="color: #f00; font-size: 20px;">&nbsp; &nbsp;
-<br/><b> Polícia e manifestantes mascarados entram em confronto em marcha do Dia do Trabalho em Paris </b></span></a>	</div>
+<script type="text/javascript">
+var persistclose=0 // Se colocar o número 0 a janela abrirá novamente. O número 1significa que depois que a barra for fechada, permanecerá fechada
+var startX = 2 // Tamanho horizontal
+var startY = 2 // Tamanho vertical
+var verticalpos="fromtop" // "fromtop" o "frombottom" define se vai em cima ou embaixo
+function iecompattest(){
+return (document.compatMode && document.compatMode!="BackCompat")? document.documentElement : document.body
+}
+function get_cookie(Name) {
+var search = Name + "="
+var returnvalue = "";
+if (document.cookie.length > 0) {
+offset = document.cookie.indexOf(search)
+if (offset != -1) {
+offset += search.length
+end = document.cookie.indexOf(";", offset);
+if (end == -1) end = document.cookie.length;
+returnvalue=unescape(document.cookie.substring(offset, end))
+}
+}
+return returnvalue;
+}
+
+function closebar(){
+if (persistclose)
+document.cookie="remainclosed=1"
+document.getElementById("topbar").style.visibility="hidden"
+}
+
+function staticbar(){
+barheight=document.getElementById("topbar").offsetHeight
+var ns = (navigator.appName.indexOf("Netscape") != -1) || window.opera;
+var d = document;
+function ml(id){
+var el=d.getElementById(id);
+if (!persistclose || persistclose && get_cookie("remainclosed")=="")
+el.style.visibility="visible"
+if(d.layers)el.style=el;
+el.sP=function(x,y){this.style.left=x+"px";this.style.top=y+"px";};
+el.x = startX;
+if (verticalpos=="fromtop")
+el.y = startY;
+else{
+el.y = ns ? pageYOffset + innerHeight : iecompattest().scrollTop +  iecompattest().clientHeight;
+el.y -= startY;
+}
+return el;
+}
+window.stayTopLeft=function(){
+if (verticalpos=="fromtop"){
+var pY = ns ? pageYOffset : iecompattest().scrollTop;
+ftlObj.y += (pY + startY - ftlObj.y)/8;
+}
+else{
+var pY = ns ? pageYOffset + innerHeight - barheight: iecompattest().scrollTop + iecompattest().clientHeight - barheight;
+ftlObj.y += (pY - startY - ftlObj.y)/8;
+}
+ftlObj.sP(ftlObj.x, ftlObj.y);
+setTimeout("stayTopLeft()", 10);
+}
+ftlObj = ml("topbar");
+stayTopLeft();
+}
+if (window.addEventListener)
+window.addEventListener("load", staticbar, false)
+else if (window.attachEvent)
+window.attachEvent("onload", staticbar)
+else if (document.getElementById)
+window.onload=staticbar
+</script>
+
+
+
+
+
