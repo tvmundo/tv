468b0d34dbf4b4448d9ea2ad5a440fe555272db0
soanatareportagens <soanatareportagens@gmail.com>
2018-05-02 10:32:14 -0300

    Update msg live.js

--- msgsvivo/msg live.js
+++ msgsvivo/msg live.js
@@ -1,2 +1,19 @@
- <div style=" padding: 6px; text-align: center;">
-<div id="responsecontainer">Urgente! bbbbbbbbbbbbbbbbbb bbbbbbbbbbbb
+<script> function abreDIV(){ document.getElementById("Layer1").style.display = 'block';	}function timedMsg(){setTimeout("abreDIV();",10000);}
+</script><body onLoad="timedMsg()">	
+<div id="Layer1" style="display:none; height:90px; width:100%px; background:#FF9900; top:15px;left:40px">
+<span style="color: #f00; font-size: 20px;">&nbsp; &nbsp;
+<br/><b> Polícia e manifestantes mascarados entram em confronto em marcha do Dia do Trabalho em Paris </b></span></a>	</div>
+<script language="JavaScript">
+function fecha_banner()
+{
+ var banner_obj = document.getElementById('banner_popup');
+ banner_obj.style.display = 'none';
+}
+setTimeout('fecha_banner()', 1000);
+</script>
+<div id="banner_popup">  </div> 
+<!--------reflasch pag ini--------------->
+<script language="javascript">
+setTimeout("location.reload();", 15000);
+</script> 
+<!--------reflasch pag fim--------------->
